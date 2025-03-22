ab92234b80620fea0b1c61276fd9b79c943ee4a1
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-03-22 01:14:19 +0000

    wip

--- test/passes/graph/transforms/verilog/test_emit_verilog_linear_mxint.py
+++ test/passes/graph/transforms/verilog/test_emit_verilog_linear_mxint.py
@@ -28,24 +28,20 @@ logger = get_logger(__name__)
 #   prefer small models for fast test
 # --------------------------------------------------
 class MLP(torch.nn.Module):
-    def __init__(self, in_features, out_features) -> None:
+    def __init__(self, features: list[int]) -> None:
         super().__init__()
 
-        self.fc1 = nn.Linear(in_features, out_features)
-        self.fc2 = nn.Linear(out_features, out_features)
-        self.fc3 = nn.Linear(out_features, out_features)
-        self.fc4 = nn.Linear(out_features, out_features)
+        layers = []
+        for in_f, out_f in zip(features[:-1], features[1:]):
+            layers.append(nn.Linear(in_f, out_f))
+            layers.append(nn.ReLU())
+
+        self.model = nn.Sequential(*layers)
+        print(self.model)
 
     def forward(self, x):
-        l1 = (
-            torch.relu(
-                self.fc1(x)
-            )
-        )
-        l2 = torch.relu(self.fc2(l1))
-        l3 = torch.relu(self.fc2(l2))
-        l4 = torch.relu(self.fc2(l3))
-        return l4
+        return  self.model(x)
+
 
 
 def test_emit_verilog_mxint(seed: int = 10):
@@ -53,27 +49,31 @@ def test_emit_verilog_mxint(seed: int = 10):
     random.seed(seed)
 
     # size of the overall block is given by block_size * batch_parallelism
-    block_size = random.randint(1,10)          # block dim 0
-    batch_parallelism = random.randint(1,10)   # block dim 1
+    block_size = random.randint(2,10)          # block dim 0
+    batch_parallelism = random.randint(2,10)   # block dim 1
+
+    mlp_depth = random.randint(1,10)
+    
+    mlp_features = [block_size * random.randint(1,10) for _ in range(mlp_depth+1)]
 
     IN_FEATURES = block_size * random.randint(1, 10)
     OUT_FEATURES = block_size * random.randint(1, 10)
     m_width = random.randint(4, 10)
-    e_width = random.randint(4, min(m_width, 10))
+    e_width = random.randint(4, min(m_width-1, 10))
 
     # number of batches to be processed by the hw block at one time
     batches = batch_parallelism * random.randint(1, 20)
     # number of times to send a set of batches to the hardware module
     num_batches = random.randint(1,20) 
     logger.info(
-        f"{block_size=}, {batch_parallelism=}, {IN_FEATURES=}, {OUT_FEATURES=}, {m_width=}, {e_width=}, {batches=}"
+        f"{block_size=}, {batch_parallelism=}, {m_width=}, {e_width=}, {batches=}\n{mlp_features=}"
     )
 
-    mlp = MLP(IN_FEATURES, OUT_FEATURES)
+    mlp = MLP(mlp_features)
     mg = chop.MaseGraph(model=mlp)
 
     # Provide a dummy input for the graph so it can use for tracing
-    x = torch.randn((batches, IN_FEATURES))
+    x = torch.randn((batches, mlp_features[0]))
     dummy_in = {"x": x}
 
     mg, _ = passes.init_metadata_analysis_pass(mg, None)
@@ -128,7 +128,7 @@ def test_emit_verilog_mxint(seed: int = 10):
 
     mg, _ = passes.add_hardware_metadata_analysis_pass(mg)
     mg, _ = passes.report_node_hardware_type_analysis_pass(mg)
-    mg, _ = passes.report_node_meta_param_analysis_pass(mg)
+    # mg, _ = passes.report_node_meta_param_analysis_pass(mg)
 
     mg, _ = passes.emit_verilog_top_transform_pass(mg)
     mg, _ = passes.emit_bram_transform_pass(mg)
@@ -136,7 +136,7 @@ def test_emit_verilog_mxint(seed: int = 10):
     mg, _ = passes.emit_cocotb_transform_pass(
         mg,
         pass_args={
-            "wait_time": 10 * num_batches,
+            "wait_time": 10 * block_size * batch_parallelism * num_batches,
             "wait_unit": "us",
             "num_batches": num_batches,
         },
@@ -151,6 +151,146 @@ def test_emit_verilog_mxint(seed: int = 10):
 
     logger.info(
         f"{block_size=}, {batch_parallelism=}, {IN_FEATURES=}, {OUT_FEATURES=}, {m_width=}, {e_width=}, {batches=}"
+    )
+
+def test_emit_verilog_mxint_mlp(seed: int = 10):
+    torch.manual_seed(seed)
+    random.seed(seed)
+
+    block_size = random.randint(2, 10)
+    batch_parallelism = random.randint(2, 10)
+    mlp_depth = random.randint(1, 10)
+    mlp_features = [block_size * random.randint(1, 10) for _ in range(mlp_depth + 1)]
+
+    params = {
+        "seed": seed,
+        "block_size": block_size,
+        "batch_parallelism": batch_parallelism,
+        "m_width": random.randint(4, 10),
+        "e_width": random.randint(4, min(9, 10)),  # min of m_width - 1 and 10
+        "batches": batch_parallelism * random.randint(1, 20),
+        "num_batches": random.randint(1, 20),
+    }
+
+    mlp = MLP(mlp_features)
+    input_shape = (mlp_features[0],)
+    logger.info(f"{block_size=}, {batch_parallelism=}, {params['e_width']=}, {params['m_width']=}, {params['batches']=}")
+
+    shared_emit_verilog_mxint(mlp, input_shape, params)
+
+def test_emit_verilog_mxint_linear(seed: int = 10):
+    torch.manual_seed(seed)
+    random.seed(seed)
+
+    block_size = random.randint(2, 10)
+    batch_parallelism = random.randint(2, 10)
+    IN_FEATURES = block_size * random.randint(1, 10)
+    OUT_FEATURES = block_size * random.randint(1, 10)
+
+    params = {
+        "seed": seed,
+        "block_size": block_size,
+        "batch_parallelism": batch_parallelism,
+        "m_width": random.randint(4, 10),
+        "e_width": random.randint(4, min(9, 10)),
+        "batches": batch_parallelism * random.randint(1, 20),
+        "num_batches": random.randint(1, 20),
+    }
+
+    linear = torch.nn.Linear(IN_FEATURES, OUT_FEATURES)
+    input_shape = (IN_FEATURES,)
+    logger.info(f"{block_size=}, {batch_parallelism=}, {params['e_width']=}, {params['m_width']=}, {params['batches']=}")
+
+    shared_emit_verilog_mxint(linear, input_shape, params)
+
+
+def shared_emit_verilog_mxint(model, input_shape, params: dict):
+    # Set seeds
+    torch.manual_seed(params["seed"])
+    random.seed(params["seed"])
+
+    block_size = params["block_size"]
+    batch_parallelism = params["batch_parallelism"]
+    m_width = params["m_width"]
+    e_width = params["e_width"]
+    batches = params["batches"]
+    num_batches = params["num_batches"]
+
+
+    mg = chop.MaseGraph(model=model)
+    x = torch.randn((batches, *input_shape))
+    dummy_in = {"x": x}
+
+    mg, _ = passes.init_metadata_analysis_pass(mg, None)
+    mg, _ = passes.add_common_metadata_analysis_pass(mg, {"dummy_in": dummy_in})
+
+    quan_args = {
+        "by": "type",
+        "default": {
+            "config": {
+                "name": "mxint",
+                "data_in_width": m_width,
+                "data_in_exponent_width": e_width,
+                "data_in_block_size": [batch_parallelism, block_size],
+                "weight_width": m_width,
+                "weight_exponent_width": e_width,
+                "weight_block_size": [block_size, block_size],
+                "bias_width": m_width,
+                "bias_exponent_width": e_width,
+                "bias_block_size": [1, block_size],
+            }
+        },
+    }
+
+    mg, _ = passes.quantize_transform_pass(mg, quan_args)
+    _ = report_node_type_analysis_pass(mg)
+
+    # Parallelism adjustments
+    for node in mg.fx_graph.nodes:
+        node_meta = node.meta["mase"].parameters["common"]
+        args = node_meta["args"]
+        results = node_meta["results"]
+        match node_meta["mase_op"]:
+            case "linear":
+                args["data_in_0"]["parallelism_0"] = block_size
+                args["data_in_0"]["parallelism_1"] = batch_parallelism
+                args["weight"]["parallelism_0"] = block_size
+                args["weight"]["parallelism_1"] = block_size
+                args["bias"]["parallelism_0"] = block_size
+                args["bias"]["parallelism_1"] = 1
+
+                results["data_out_0"]["parallelism_0"] = block_size
+                results["data_out_0"]["parallelism_1"] = batch_parallelism
+            case 'relu':
+                args["data_in_0"]["parallelism_0"] = block_size
+                args["data_in_0"]["parallelism_1"] = batch_parallelism
+                results["data_out_0"]["parallelism_0"] = block_size
+                results["data_out_0"]["parallelism_1"] = batch_parallelism
+
+    mg, _ = passes.add_hardware_metadata_analysis_pass(mg)
+    mg, _ = passes.report_node_hardware_type_analysis_pass(mg)
+
+    mg, _ = passes.emit_verilog_top_transform_pass(mg)
+    mg, _ = passes.emit_bram_transform_pass(mg)
+    mg, _ = passes.emit_internal_rtl_transform_pass(mg)
+    mg, _ = passes.emit_cocotb_transform_pass(
+        mg,
+        pass_args={
+            "wait_time": 10 * block_size * batch_parallelism * num_batches,
+            "wait_unit": "us",
+            "num_batches": num_batches,
+        },
+    )
+
+    simulate(
+        skip_build=False,
+        skip_test=False,
+        simulator="verilator",
+        waves=True,
+    )
+
+    logger.info(
+        f"{block_size=}, {batch_parallelism=}, {m_width=}, {e_width=}, {batches=}"
     )
 
 
